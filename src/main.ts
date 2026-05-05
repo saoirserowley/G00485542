2999e5aadc16da3a97ddfd0bd31f68a027d8b607
saoirserowley <g00485542@atu.ie>
2026-05-05 13:29:04 +0100

    home page working with trending movies and search after changes made to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+// main.ts
+// This is the entry point of the app, it bootstraps the root component
+// provideHttpClient is needed so we can make HTTP requests to the TMDB API
+// https://angular.dev/guide/http/making-requests
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
@@ -10,5 +14,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    // this line is what allows HttpClient to work throughout the whole app
+    provideHttpClient(),
   ],
 });
