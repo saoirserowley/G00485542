a9fba61944dfd6326001bd9e2c7aee02d2421fef
saoirserowley <g00485542@atu.ie>
2026-05-05 10:45:49 +0100

    initial project setup with pages generated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,16 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'movie-details',
+    loadComponent: () => import('./movie-details/movie-details.page').then( m => m.MovieDetailsPage)
+  },
+  {
+    path: 'details',
+    loadComponent: () => import('./details/details.page').then( m => m.DetailsPage)
+  },
+  {
+    path: 'favourites',
+    loadComponent: () => import('./favourites/favourites.page').then( m => m.FavouritesPage)
+  },
 ];
